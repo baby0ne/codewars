a520cd4a94a4a9c840be04d9868154e2b148f532
Андрей Ашмянец <andreka6033@gmail.com>
2022-01-06 15:34:29 +0300

    Delete SquareEveryDigit.js

--- SquareEveryDigit.js
+++
@@ -1,8 +0,0 @@
-function squareDigits(num) {
-   let oldString = String(num);
-   let newString = "";
-   for (let i = 0; i < oldString.length; i++){
-      newString += Number(oldString[i]) * Number(oldString[i]);
-   }
-   return Number(newString);
-}
