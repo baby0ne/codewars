b7737ae8ef3814713067d49858a3713960065cc2
Андрей Ашмянец <andreka6033@gmail.com>
2022-01-07 14:58:37 +0300

    Delete TwotoOne.js

--- TwotoOne.js
+++
@@ -1,7 +0,0 @@
-function longest(s1, s2) {
-   return (s1 + s2).split('').filter(onlyUnique).sort().join('')
-}
-
-function onlyUnique(value, index, self) {
-   return self.indexOf(value) === index;
-} 
