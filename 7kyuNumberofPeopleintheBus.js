710c9862ac269ae54ce20f33783058ef8cc76369
Андрей Ашмянец <andreka6033@gmail.com>
2022-02-04 15:20:23 +0300

    Delete 7kyuNumberofPeopleintheBus.js

--- 7kyuNumberofPeopleintheBus.js
+++
@@ -1,8 +0,0 @@
-var number = function (busStops) {
-   let counter = 0;
-   for (let i = 0; i < busStops.length; i++){
-      counter += busStops[i][0];
-      counter -= busStops[i][1];
-   }
-   return counter;
-}
