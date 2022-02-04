23cb97aa450ff7045c4ca1ec4ff61f00459f9dd8
Андрей Ашмянец <andreka6033@gmail.com>
2022-02-04 15:17:37 +0300

    Delete HolidayDutyFree.js

--- HolidayDutyFree.js
+++
@@ -1,9 +0,0 @@
-function dutyFree(normPrice, discount, hol) {
-   let discountInPercent = discount / 100;
-   let saveMoney = normPrice * discountInPercent;
-   let bottleNumber; 
-   for (bottleNumber = 0; hol > saveMoney; bottleNumber++){
-      hol -= saveMoney;
-   }
-   return bottleNumber;
-}
