082ac7244a67944816691172d438001e18cdff0c
Андрей Ашмянец <andreka6033@gmail.com>
2022-02-04 15:20:39 +0300

    Delete 7kyuFindtheaverageage.js

--- 7kyuFindtheaverageage.js
+++
@@ -1,9 +0,0 @@
-function getAverageAge(list) {
-   let sumAge = 0;
-
-   for (let i = 0; i < list.length; i++) {
-      sumAge += list[i].age;
-   }
-
-   return Math.round(sumAge / list.length);
-}
