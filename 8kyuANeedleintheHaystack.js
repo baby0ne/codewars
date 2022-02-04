ca63addb3bf123bda1baba73b5f35ff4e4a9ad3f
Андрей Ашмянец <andreka6033@gmail.com>
2022-02-04 15:18:21 +0300

    Delete 8kyuANeedleintheHaystack.js

--- 8kyuANeedleintheHaystack.js
+++
@@ -1,7 +0,0 @@
-function findNeedle(haystack) {
-   for(let i = 0; i < haystack.length; i++){
-      if(haystack[i] === "needle"){
-         return 'found the needle at position ' + i;
-      }
-   }
-}
