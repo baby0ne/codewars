03d1916183a0e478c4b3ed8dbd78f50e7a6a13da
Андрей Ашмянец <andreka6033@gmail.com>
2022-02-04 15:18:53 +0300

    Create RockPaperScissors.js

---
+++ 8kyu/RockPaperScissors.js
@@ -0,0 +1,18 @@
+const rps = (p1, p2) => {
+   const getMsg = (n) => `Player ${n} won!`;
+   if (p1 === p2) {
+      return "Draw!";
+   } else if (p1 === "scissors" && p2 === "paper"){
+      return getMsg(1);
+   } else if (p1 === "paper" && p2 === "scissors"){
+      return getMsg(2);
+   } else if (p1 === "rock" && p2 === "paper") {
+      return getMsg(2);
+   } else if (p1 === "paper" && p2 === "rock") {
+      return getMsg(1);
+   } else if (p1 === "scissors" && p2 === "rock") {
+      return getMsg(2);
+   } else if (p1 === "rock" && p2 === "scissors") {
+      return getMsg(1);
+   }
+};
