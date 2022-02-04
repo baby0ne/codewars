50d85bc4e816106db8b23d4eaf988b12fc2def3f
Андрей Ашмянец <andreka6033@gmail.com>
2022-02-04 15:19:33 +0300

    Create CountingSheep.js

---
+++ 8kyu/CountingSheep.js
@@ -0,0 +1,11 @@
+function countSheeps(arrayOfSheep) {
+   let count = 0;
+
+   for(let i = 0; i < arrayOfSheep.length; i++){
+      if (arrayOfSheep[i] === true){
+         count ++;
+      }
+   }
+   
+   return count;
+}
