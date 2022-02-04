4ba040007543ce757dd2efa6925cd0bda00deec1
Андрей Ашмянец <andreka6033@gmail.com>
2022-02-04 15:16:19 +0300

    Create 7kyuVowelCount.js

---
+++ 7kyu/7kyuVowelCount.js
@@ -0,0 +1,11 @@
+function getCount(str) {
+   var vowelsCount = 0;
+   
+   for(let i = 0; i < str.length; i++){
+      if (str[i] === "a" || str[i] === "e" || str[i] === "i" || str[i] === "o" || str[i] === "u"){
+         vowelsCount++;
+      }
+   }
+
+   return vowelsCount;
+}
