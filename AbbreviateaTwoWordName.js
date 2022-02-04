43a005509f4c2e0709a8bddfbf5aaf4f8cb1d2ba
Андрей Ашмянец <andreka6033@gmail.com>
2022-02-04 15:18:19 +0300

    Delete AbbreviateaTwoWordName.js

--- AbbreviateaTwoWordName.js
+++
@@ -1,9 +0,0 @@
-function abbrevName(name) {
-   let letterLastName = "";
-   for (let i = 0; i < name.length; i++){
-      if(name[i] == " "){
-         letterLastName = name[i + 1];
-      }
-   }
-   return name[0].toUpperCase() + "." + letterLastName.toUpperCase();
-}
