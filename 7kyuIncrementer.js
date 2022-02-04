4e022b9dc187874315e874e12c6a41f2d14aae39
Андрей Ашмянец <andreka6033@gmail.com>
2022-02-04 15:20:33 +0300

    Delete 7kyuIncrementer.js

--- 7kyuIncrementer.js
+++
@@ -1,9 +0,0 @@
-function incrementer(nums) {
-   for (let i = 0; i < nums.length; i++){
-      nums[i] += (i+1); 
-      if (nums[i] > 9){
-         nums[i] = nums[i]%10;
-      }
-   }
-   return nums;
-}
