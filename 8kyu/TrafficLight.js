50e820a22708e5dde00dadd26fecdfc3fc8d2da6
Андрей Ашмянец <andreka6033@gmail.com>
2022-02-04 15:18:36 +0300

    Create TrafficLight.js

---
+++ 8kyu/TrafficLight.js
@@ -0,0 +1,9 @@
+function updateLight(current) {
+   if (current === "green"){
+      return "yellow";
+   } else if (current === "yellow") {
+      return "red";
+   } else if (current === "red") {
+      return "green";
+   }
+}
