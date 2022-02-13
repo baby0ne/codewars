5f9801ed161d334bb2b3d3728492306e3fd4ecf9
Андрей Ашмянец <andreka6033@gmail.com>
2022-02-13 11:43:03 +0300

    Create FindTheFirstPythonDeveloper.js

---
+++ 7kyu/FindTheFirstPythonDeveloper.js
@@ -0,0 +1,9 @@
+function getFirstPython(list) {
+   for (let i = 0; i < list.length; i++){
+      if (list[i].language === 'Python'){
+         return `${list[i].firstName}, ${list[i].country}`;
+      }
+   }
+
+   return 'There will be no Python developers';
+}
