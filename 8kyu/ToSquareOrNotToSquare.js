7028dcbb291d26cb40cb9117b13b6293cdc491d1
Андрей Ашмянец <andreka6033@gmail.com>
2022-04-12 23:49:32 +0300

    Create ToSquareOrNotToSquare.js

---
+++ 8kyu/ToSquareOrNotToSquare.js
@@ -0,0 +1,8 @@
+function squareOrSquareRoot(array) {
+   let resultArr = [];
+   for(let i = 0; i < array.length; i++) {
+      if (Number.isInteger(Math.sqrt(array[i]))) resultArr.push(array[i])
+      else resultArr.push(array[i] * array[i])
+   }
+   return resultArr;
+}
