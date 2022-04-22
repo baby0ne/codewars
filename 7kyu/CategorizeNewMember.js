de286474abc403753492139a113e44ab3c7dcb7d
Андрей Ашмянец <andreka6033@gmail.com>
2022-04-22 14:29:09 +0300

    Create CategorizeNewMember.js

---
+++ 7kyu/CategorizeNewMember.js
@@ -0,0 +1,10 @@
+function openOrSenior(data) {
+   let result = [];
+
+   for (let i = 0; i < data.length; i++) {
+      if (data[i][0] >= 55 && data[i][1] > 7) result.push('Senior')
+      else result.push('Open');
+   }
+
+   return result;
+}
