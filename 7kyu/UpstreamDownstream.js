f8413ddcf5fda312e811ff358969bd7f3c226920
Андрей Ашмянец <andreka6033@gmail.com>
2022-06-24 23:29:27 +0300

    Update UpstreamDownstream.js

--- 7kyu/UpstreamDownstream.js
+++ 7kyu/UpstreamDownstream.js
@@ -1,6 +1,5 @@
 function time(distance, boatSpeed, stream) {
    if (stream[0] === 'D') boatSpeed = boatSpeed + Number(stream.split(' ')[1])
    else boatSpeed = boatSpeed - Number(stream.split(' ')[1])
-   debugger
    return Number((distance / boatSpeed).toFixed(2));
 }
