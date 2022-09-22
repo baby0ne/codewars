92f326c13fbc02f27f30dcc2c1317c325bef4e79
Андрей Ашмянец <andreka6033@gmail.com>
2022-09-22 19:00:11 +0300

    new kata v6

---
+++ 6kyu/StopGninnipSMySdroW.js
@@ -0,0 +1,13 @@
+function spinWords(string){
+    debugger;
+    let arr1 = string.split(' ');
+
+    for(let i = 0; i < arr1.length; i++){
+        if(arr1[i].length > 4){
+            let arr2 = arr1[i].split('').reverse();
+            arr1[i] = arr2.join();
+        }
+    }
+
+    return arr1.join(' ');
+}
